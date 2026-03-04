78a10628fb406bd91eba1d63d9b6b69a72fcd33b
Andrei Karpov <andrei.karpov@ivkhk.ee>
2026-03-04 14:57:53 +0200

    AAA

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['apiservice-2ec4.onrender.com']
+  }
 })
